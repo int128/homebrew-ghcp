22da3f62e4466432267e31c5eb3a9f3b2040435b
Hidetake Iwata <int128@gmail.com>
2019-07-26 19:22:43 +0900

    v1.5.0

--- ghcp.rb
+++ ghcp.rb
@@ -1,9 +1,9 @@
 class Ghcp < Formula
   desc "Copy files to a repository using GitHub API"
   homepage "https://github.com/int128/ghcp"
-  url "https://github.com/int128/ghcp/releases/download/v1.4.0/ghcp_darwin_amd64"
-  version "v1.4.0"
-  sha256 "8b241804fe2ac2e1c1c3f4c83f729a2da5df726665f27018b296797012d2a0bd"
+  url "https://github.com/int128/ghcp/releases/download/v1.5.0/ghcp_darwin_amd64"
+  version "v1.5.0"
+  sha256 "540783e2080d249092b53b7c9bfe1b1424c8899d261c7e4c271ca487518f7fc2"
 
   def install
     bin.install "ghcp_darwin_amd64" => "ghcp"
