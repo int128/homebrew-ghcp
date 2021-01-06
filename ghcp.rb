fd92f79f58969836a730aa2ee2be869072cee418
Hidetake Iwata <int128@gmail.com>
2021-01-06 16:12:15 +0900

    Bump the version to v1.9.0

--- ghcp.rb
+++ ghcp.rb
@@ -1,9 +1,9 @@
 class Ghcp < Formula
   desc "Commit files to a repository using GitHub API without git configuration"
   homepage "https://github.com/int128/ghcp"
-  url "https://github.com/int128/ghcp/releases/download/v1.8.0/ghcp_darwin_amd64.zip"
-  version "v1.8.0"
-  sha256 "7f4867f78ebf322c0460bf76a54a71e7b5eac91f18139cf440cd0fb35a835628"
+  url "https://github.com/int128/ghcp/releases/download/v1.9.0/ghcp_darwin_amd64.zip"
+  version "v1.9.0"
+  sha256 "c7e6e9c7e51154e89d7b1964eb621bce37643d5b990cda9a54cdd636f68c282e"
 
   def install
     bin.install "ghcp"
